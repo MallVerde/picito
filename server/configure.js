588c1e09f444bc741200a382e23115922d3fadd4
Jose Manuel Prieto <treason.199@gmail.com>
2017-10-17 19:04:50 -0500

    Servidor estatico listo

--- server/configure.js
+++ server/configure.js
@@ -1,4 +1,40 @@
-module.exports = function(app){
+// Cargando dependencias
+
+var path = require ('path'),
+    exphdp = require ('express-handlebars'),
+    express = require ('express'),
+    bodyParser = require ('body-parser'),
+    cookieParser = require ('cookie-parser'),
+    morgan = require ('morgan'),
+    methodOverride = require ('method-override'),
+    errorHandler = require ('errorhandler'),
+    moment = require ('moment')
+    multer = require ('multer');
+
+module.exports= function(app){
+    // Conectando Middlewares
+    // Middleware para
+    // loggin de HTTP
+    app.use(morgan('dev'));
+    // Parseo de URL
+    app.use(bodyParser.urlencoded({
+        'extended':true
+    }));
+    app.use(bodyParser.json());
+    // Compatibilidad de verbos HTTP
+    app.use(methodOverride());
+    // Parseo de Cookies
+    app.use(cookieParser('Algun-valor-secreto'));
+    // Habilitando el servicio estatico 
+    // de archivos
+    app.use ('/public/', express.static(path.join(
+        __dirname, '../public'
+    )));
+    // Middleware para el  manejo 
+    // de errores
+    if (app.get('env') === 'development'){
+        app.use(errorHandler());
+    }
     return app;
 
 }
